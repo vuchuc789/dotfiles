79402f72a34558445b60df0b1838f93d9f56c2ab
Vu Chuc <vuchuc781999@gmail.com>
2026-04-05 09:45:49 +0100

    05/04/2026

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -238,111 +238,6 @@ return {
   },
 
   {
-    "jake-stewart/multicursor.nvim",
-    branch = "1.0",
-    lazy = true,
-    event = "VeryLazy",
-    config = function()
-      local mc = require "multicursor-nvim"
-      mc.setup()
-
-      -- Mappings defined in a keymap layer only apply when there are
-      -- multiple cursors. This lets you have overlapping mappings.
-      mc.addKeymapLayer(function(layerSet)
-        -- Select a different cursor as the main one.
-        layerSet({ "n", "x" }, "<left>", mc.prevCursor)
-        layerSet({ "n", "x" }, "<right>", mc.nextCursor)
-
-        -- Delete the main cursor.
-        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
-
-        -- Enable and clear cursors using escape.
-        layerSet("n", "<esc>", function()
-          if not mc.cursorsEnabled() then
-            mc.enableCursors()
-          else
-            mc.clearCursors()
-          end
-        end)
-      end)
-
-      -- Customize how cursors look.
-      local hl = vim.api.nvim_set_hl
-      hl(0, "MultiCursorCursor", { reverse = true })
-      hl(0, "MultiCursorVisual", { link = "Visual" })
-      hl(0, "MultiCursorSign", { link = "SignColumn" })
-      hl(0, "MultiCursorMatchPreview", { link = "Search" })
-      hl(0, "MultiCursorDisabledCursor", { reverse = true })
-      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
-    end,
-  },
-
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-      provider = "gemini-flash",
-      providers = {
-        ["gemini-pro"] = {
-          __inherited_from = "gemini",
-          model = "gemini-2.5-pro",
-        },
-        ["gemini-flash"] = {
-          __inherited_from = "gemini",
-          model = "gemini-2.5-flash",
-        },
-        ["gemini-flash-lite"] = {
-          __inherited_from = "gemini",
-          model = "gemini-2.5-flash-lite",
-        },
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       {
@@ -376,4 +271,109 @@ return {
       "neovim/nvim-lspconfig",
     },
   },
+
+  -- {
+  --   "jake-stewart/multicursor.nvim",
+  --   branch = "1.0",
+  --   lazy = true,
+  --   event = "VeryLazy",
+  --   config = function()
+  --     local mc = require "multicursor-nvim"
+  --     mc.setup()
+  --
+  --     -- Mappings defined in a keymap layer only apply when there are
+  --     -- multiple cursors. This lets you have overlapping mappings.
+  --     mc.addKeymapLayer(function(layerSet)
+  --       -- Select a different cursor as the main one.
+  --       layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+  --       layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+  --
+  --       -- Delete the main cursor.
+  --       layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+  --
+  --       -- Enable and clear cursors using escape.
+  --       layerSet("n", "<esc>", function()
+  --         if not mc.cursorsEnabled() then
+  --           mc.enableCursors()
+  --         else
+  --           mc.clearCursors()
+  --         end
+  --       end)
+  --     end)
+  --
+  --     -- Customize how cursors look.
+  --     local hl = vim.api.nvim_set_hl
+  --     hl(0, "MultiCursorCursor", { reverse = true })
+  --     hl(0, "MultiCursorVisual", { link = "Visual" })
+  --     hl(0, "MultiCursorSign", { link = "SignColumn" })
+  --     hl(0, "MultiCursorMatchPreview", { link = "Search" })
+  --     hl(0, "MultiCursorDisabledCursor", { reverse = true })
+  --     hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+  --     hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+  --   end,
+  -- },
+
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   version = false, -- Never set this value to "*"! Never!
+  --   opts = {
+  --     provider = "gemini-flash",
+  --     providers = {
+  --       ["gemini-pro"] = {
+  --         __inherited_from = "gemini",
+  --         model = "gemini-2.5-pro",
+  --       },
+  --       ["gemini-flash"] = {
+  --         __inherited_from = "gemini",
+  --         model = "gemini-2.5-flash",
+  --       },
+  --       ["gemini-flash-lite"] = {
+  --         __inherited_from = "gemini",
+  --         model = "gemini-2.5-flash-lite",
+  --       },
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "echasnovski/mini.pick", -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 }
